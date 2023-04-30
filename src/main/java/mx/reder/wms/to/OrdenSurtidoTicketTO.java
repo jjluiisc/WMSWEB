6b3ae3b04dd07c9007970d83346877993854895f
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2023-04-29 23:48:41 -0600

    Ticket

---
+++ src/main/java/mx/reder/wms/to/OrdenSurtidoTicketTO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.reder.wms.to;
+
+/**
+ *
+ * @author Luis
+ */
+public class OrdenSurtidoTicketTO {
+    public String compania = "";
+    public String razonsocialcompania = "";
+    public int flsurtido = 0;
+    public String contenedor = "";
+    public String pedido = "";
+    public String cliente = "";
+    public String nombrecliente = "";
+    public String ruta = "";    
+}
